f918dbb3ff054695fe1fedb79a1863ac85591bed
3mgaines6-pixel <3mgaines6@gmail.com>
2026-01-29 11:46:02 -0500

    Update CardioStudio.js

--- screens/CardioStudio.js
+++ screens/CardioStudio.js
@@ -1,1 +1,9 @@
+export function CardioStudio() {
+  const container = document.createElement('div');
+  container.innerHTML = `
+    <h1>Cardio Studio</h1>
+    <button onclick="renderScreen('Lobby')">Back to Lobby</button>
+  `;
+  return container;
+}
 
