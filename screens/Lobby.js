5adcbd3cb099af481813b416c3bfce49fc8f98c3
3mgaines6-pixel <3mgaines6@gmail.com>
2026-01-29 10:43:04 -0500

    Add Lobby component with initial HTML structure

--- screens/Lobby.js
+++ screens/Lobby.js
@@ -1,1 +1,9 @@
+export function Lobby() {
+  const container = document.createElement('div');
+  container.innerHTML = `
+    <h1>Lobby</h1>
+    <p>The app is working!</p>
+  `;
+  return container;
+}
 
