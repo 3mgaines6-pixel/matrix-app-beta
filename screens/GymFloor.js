dbd6d3ad7a76f9d95363fddbc539081575dd1af2
3mgaines6-pixel <3mgaines6@gmail.com>
2026-01-30 17:30:18 -0500

    Update GymFloor.js

--- screens/GymFloor.js
+++ screens/GymFloor.js
@@ -1,7 +1,8 @@
 export function Lobby() {
   const container = document.createElement('div');
   container.innerHTML = `
-    <h1>Lobby</h1>
+   <h1>Gym Floor</h1>
+
     <button onclick="renderScreen('CardioStudio')">Cardio Studio</button>
     <button onclick="renderScreen('StrengthStudio')">Strength Studio</button>
     <button onclick="renderScreen('StretchStudio')">Stretch Studio</button>
