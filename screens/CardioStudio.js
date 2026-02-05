1306ff7fc6a363377d361a46a222fbb7e60cab8e
3mgaines6-pixel <3mgaines6@gmail.com>
2026-02-05 15:14:28 -0500

    Update CardioStudio.js

--- screens/CardioStudio.js
+++ screens/CardioStudio.js
@@ -2,7 +2,7 @@ export function CardioStudio() {
   const container = document.createElement('div');
   container.innerHTML = `
     <h1>Cardio Studio</h1>
-    <button onclick="renderScreen('Gym floor')">Back to Lobby</button>
+    <button onclick="renderScreen('Gym floor')">Back to GYM FLOOR</button>
   `;
   return container;
 }
