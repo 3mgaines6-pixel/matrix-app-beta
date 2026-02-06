9b24f5fbc8bd2345fd1b60943145494dccbb56bd
3mgaines6-pixel <3mgaines6@gmail.com>
2026-02-05 20:31:14 -0500

    Update Machine-5.js

--- screens/Machine-5.js
+++ screens/Machine-5.js
@@ -5,9 +5,6 @@
 export function Machine5() {
   const container = document.createElement("div");
   container.className = "machine-screen";
-container.innerHTML = `
-    <h1 class="machine-title">#5 Seated Row</h1>
-    <button class="close-btn" onclick="renderScreen('StrengthStudio')">Close</button>
   `;
 
   /* ---------- HEADER ---------- */
@@ -125,3 +122,10 @@ container.innerHTML = `
 
   return container;
 }
+
+
+
+
+
+
+
