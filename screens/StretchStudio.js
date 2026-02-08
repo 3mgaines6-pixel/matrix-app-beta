97089364f961799f5195607a1d2b8c6519de9bf3
3mgaines6-pixel <3mgaines6@gmail.com>
2026-02-08 17:01:11 -0500

    Delete screens directory

--- screens/StretchStudio.js
+++
@@ -1,9 +0,0 @@
-export function StretchStudio() {
-  const container = document.createElement('div');
-  container.innerHTML = `
-    <h1>Stretch Studio</h1>
-    <button onclick="renderScreen('Lobby')">Back to Lobby</button>
-  `;
-  return container;
-}
-
