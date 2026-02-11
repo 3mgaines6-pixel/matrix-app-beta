896e9b28691596729893fad11bbb2d9a5c7e13c2
3mgaines6-pixel <3mgaines6@gmail.com>
2026-02-11 16:35:16 -0500

    Delete weeklySchedule.js

--- weeklySchedule.js
+++
@@ -1,7 +0,0 @@
-export const WEEKLY_SCHEDULE = {
-  Monday:    [1, 2, 3, 4, 5],      // Heavy Upper
-  Tuesday:   [6, 7, 8, 9, 10],     // Heavy Lower
-  Wednesday: [1, 6, 2, 7, 11],     // Full Body
-  Thursday:  [7, 8, 9, 10],        // Light Lower
-  Friday:    [1, 2, 3, 4, 5],      // Upper Light Pump
-};
