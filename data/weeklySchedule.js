fef61debc91bc4a5991a2d97636f7a42c2ee44f2
3mgaines6-pixel <3mgaines6@gmail.com>
2026-02-12 10:57:10 -0500

    Update weeklySchedule.js

--- data/weeklySchedule.js
+++ data/weeklySchedule.js
@@ -1,47 +1,41 @@
 export const WEEKLY_SCHEDULE = {
   Mon: [
     15,   // Leg Press (Heavy)
-    12,   // Seated Leg Curl
-    10,   // Prone Leg Curl
-    11,   // Leg Extension
-    13,   // Hip Adductor
-    14    // Hip Abductor
+    12,   // Seated Leg Curl (Light)
+    13,   // Hip Adductor (Light)
+    14,   // Hip Abductor (Light)
+    3     // Ab Crunch (Core)
   ],
 
   Tue: [
     7,    // Chest Press (Heavy)
-    6,    // Shoulder Press (Heavy)
-    2,    // Triceps Press
-    9,    // Pec Fly / Rear Delt
-    8,    // Lat Pulldown
-    5     // Seated Row
+    8,    // Lat Pulldown (Heavy)
+    5,    // Seated Row (Light)
+    6,    // Shoulder Press (Light)
+    2     // Triceps Press (Light)
   ],
 
   Wed: [
+    15,   // Leg Press (Heavy)
     107,  // Chest Press (Light)
-    2,    // Triceps Press
-    9,    // Pec Fly / Rear Delt
-    8,    // Lat Pulldown
-    5,    // Seated Row
-    4,    // Back Extension
-    1,    // Dependent Curl
+    5,    // Seated Row (Light)
+    14,   // Hip Abductor (Light)
     3     // Ab Crunch (Core)
   ],
 
   Thur: [
     115,  // Leg Press (Light)
-    12,   // Seated Leg Curl
-    10,   // Prone Leg Curl
-    11,   // Leg Extension
-    13,   // Hip Adductor
-    14    // Hip Abductor
+    12,   // Seated Leg Curl (Light)
+    13,   // Hip Adductor (Light)
+    14,   // Hip Abductor (Light)
+    4     // Back Extension (Core)
   ],
 
   Fri: [
-    8,    // Lat Pulldown
-    5,    // Seated Row
-    9,    // Pec Fly / Rear Delt
-    1,    // Dependent Curl
-    3     // Ab Crunch (Core)
+    5,    // Seated Row (Light)
+    6,    // Shoulder Press (Light)
+    9,    // Pec Fly / Rear Delt (Light)
+    2,    // Triceps Press (Light)
+    1     // Dependent Curl (Light)
   ]
 };
