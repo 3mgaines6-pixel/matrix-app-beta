b7cd7376347a0368148bfd92fee371578df96621
3mgaines6-pixel <3mgaines6@gmail.com>
2026-02-24 21:11:49 -0500

    Create weekly.js

---
+++ data/weekly.js
@@ -0,0 +1,43 @@
+
+
+export const WEEKLY_SCHEDULE = {
+  Mon: [
+    15,   // Leg Press (Heavy)
+    12,   // Seated Leg Curl (Light)
+    13,   // Hip Adductor (Light)
+    14,   // Hip Abductor (Light)
+    3     // Ab Crunch (Core)
+  ],
+
+  Tue: [
+    7,    // Chest Press (Heavy)
+    8,    // Lat Pulldown (Heavy)
+    5,    // Seated Row (Light)
+    6,    // Shoulder Press (Light)
+    2     // Triceps Press (Light)
+  ],
+
+  Wed: [
+    15,   // Leg Press (Heavy)
+    107,  // Chest Press (Light)
+    5,    // Seated Row (Light)
+    14,   // Hip Abductor (Light)
+    3     // Ab Crunch (Core)
+  ],
+
+  Thur: [
+    115,  // Leg Press (Light)
+    12,   // Seated Leg Curl (Light)
+    13,   // Hip Adductor (Light)
+    14,   // Hip Abductor (Light)
+    4     // Back Extension (Core)
+  ],
+
+  Fri: [
+    5,    // Seated Row (Light)
+    6,    // Shoulder Press (Light)
+    9,    // Pec Fly / Rear Delt (Light)
+    2,    // Triceps Press (Light)
+    1     // Dependent Curl (Light)
+  ]
+};
